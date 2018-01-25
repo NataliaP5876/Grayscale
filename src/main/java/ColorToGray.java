711a71e1b055a7b0a362c708102c5af5b8862d6e
Natalia Ponomareva <NataliaP5876@gmail.com>
2018-01-25 23:07:10 +0600

    добавлен GUI
    создан GUI, открытие изображения, добавлен фильтр для выбираемых файлов.

--- src/main/java/ColorToGray.java
+++ src/main/java/ColorToGray.java
@@ -2,10 +2,10 @@
  * Created by Vol4onka on 23.01.2018.
  */
 public class ColorToGray {
-    public static int colorToGray(int R, int G, int B){
-        double linR = Linearizer.linearize(R);
-        double linG = Linearizer.linearize(G);
-        double linB = Linearizer.linearize(B);
+    public static int colorToGray(int r, int g, int b){
+        double linR = Linearizer.linearize(r);
+        double linG = Linearizer.linearize(g);
+        double linB = Linearizer.linearize(b);
         double lin_ysRGB = 0;
         int ysRGB =0;
         lin_ysRGB = 0.2126*linR+0.7152*linG +0.0722*linB;
--- src/main/java/GUI_original.java
+++ src/main/java/GUI_original.java
@@ -1,54 +1,138 @@
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class GUI_original extends JFrame {
-    private JLabel input = new JLabel("Исходное изображение:");
+    private JLabel input = new JLabel("");
     private JPanel img1 = new JPanel();
     private JPanel img2 = new JPanel();
-    private JLabel output = new JLabel("Результат:");
-    private JButton start = new JButton("Запустить");
-    private JButton fileopen = new JButton("Выбрать Файл");
+    private JLabel picLabel1 = new JLabel();
+    private JLabel picLabel2 = new JLabel();
+    private JLabel output = new JLabel("");
+    private JButton start = new JButton("Start");
+    private JButton fileopen = new JButton("Select Image");
+    private String pathInput;
+    private String pathOutput;
 
-    public GUI_original(){
+
+    public GUI_original() {
         super("HM Grayscale");
-        this.setBounds(200,200,500,200);
+        this.setBounds(200,200,1200,800);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         Container container = this.getContentPane();
         container.setLayout(new GridLayout(1,2,2,2));
-
         JPanel col1 = new JPanel();
-        col1.setLayout(new GridLayout(3,1,2,2));
-        col1.add(input);
-        col1.add(img1);
-        col1.add(img2);
-        img1.setBackground(Color.white);
-        img1.setPreferredSize(new Dimension(200, 200));
+        col1.setLayout(new GridBagLayout());
+
         JPanel col2 = new JPanel();
+        col2.setLayout(new GridBagLayout());
+
+        GridBagConstraints c = new GridBagConstraints();
+        c.weightx = 0.5;
+        c.weighty = 0.3;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 0;
+        col1.add(input, c);
+
+        c.weightx = 0.5;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 0;
+        col2.add(output, c);
+
+        c.weightx = 0.5;
+        c.weighty = 3;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 1;
+
+        img1.add(picLabel1);
+        col1.add(img1, c);
+
+        c.weightx = 0.5;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 1;
+        img2.add(picLabel2);
+        col2.add(img2, c);
+
+        c.weightx = 0.5;
+        c.weighty = 0.3;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 2;
+        col1.add(fileopen, c);
+
+        c.weightx = 0.5;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 2;
+        col2.add(start, c);
 
         container.add(col1);
         container.add(col2);
 
-        /*container.add(input);
-        container.add(output);
-        container.add(img1);
-        container.add(img2);
-        img1.setBackground(Color.white);
+        fileopen.addActionListener(new OpenFile());
+        start.addActionListener(new StartGrayscale());
+    }
 
-        img1.setPreferredSize(new Dimension(200, 200));
-        fileopen.addActionListener(new StartGrayscale());
+    class OpenFile implements ActionListener {
+        public void actionPerformed (ActionEvent e){
+            JFileChooser fileopen = new JFileChooser("C:\\Users\\Vol4onka\\Desktop\\image\\test");
+            fileopen.setFileHidingEnabled(false);
+            fileopen.setAcceptAllFileFilterUsed(false);
+            String[] extensions= {"png","jpg","jpeg"};
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("image (*.png, *.jpg, *.jpeg)",extensions);
+            fileopen.setFileFilter(filter);
 
-        container.add(fileopen);
-        container.add(start);*/
-
+            int ret = fileopen.showDialog(null, "Open image");
+            if (ret == JFileChooser.APPROVE_OPTION) {
+                File file = fileopen.getSelectedFile();
+                pathInput = file.getPath();
+                //img1 = file.;
+                BufferedImage myPicture = null;
+                try {
+                    myPicture = ImageIO.read(new File(pathInput));
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+                picLabel1.setIcon(new ImageIcon(myPicture));
+                input.setText("Original image:");
+            }
+        }
     }
 
     class StartGrayscale implements ActionListener {
         public void actionPerformed (ActionEvent e){
-            GUI_openFile app2 = new GUI_openFile();
-            app2.setVisible(true);
+            JFileChooser fileSaveDirectoty = new JFileChooser("C:\\Users\\Vol4onka\\Desktop\\image\\test");
+            fileSaveDirectoty.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+            int ret = fileSaveDirectoty.showDialog(null,"Select directory");
+            if (ret == JFileChooser.APPROVE_OPTION) {
+                File file = fileSaveDirectoty.getSelectedFile();
+                pathOutput = file.getPath()+"\\Output.jpg";
+                try {
+                    Grayscale.grayscale(pathInput, pathOutput);
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+                BufferedImage myPicture = null;
+                try {
+                    myPicture = ImageIO.read(new File(pathOutput));
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+                picLabel2.setIcon(new ImageIcon(myPicture));
+                output.setText("Result:");
+            }
+
         }
     }
 }
